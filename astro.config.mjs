3e2e83798c4b75c61d55c1cbc54ae02e0e3de8aa
jon kurinsky <jkurinsky@fastmail.com>
2022-10-17 09:01:21 -0400

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
+
+// https://astro.build/config
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     server: {
-      open: true
-    }
+      open: true,
+    },
   },
-  integrations: [mdx()]
+  site: 'https://krnsk0.dev',
+  integrations: [mdx(), sitemap()],
 });
