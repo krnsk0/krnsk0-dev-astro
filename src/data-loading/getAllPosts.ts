3b29e78ea963f4d93ef0b0725a6eeb7574835ee8
jon kurinsky <jkurinsky@fastmail.com>
2022-10-16 22:07:09 -0400

    better typechecking

--- src/data-loading/getAllPosts.ts
+++ src/data-loading/getAllPosts.ts
@@ -1,5 +1,15 @@
 import { Frontmatter, FrontmatterSchema } from './postTypes';
 
+function isObject(x: unknown): x is Record<string, unknown> {
+  return typeof x === 'object' && x !== null;
+}
+
+function hasProp<K extends PropertyKey>(
+  data: object,
+  prop: K
+): data is Record<K, unknown> {
+  return prop in data;
+}
 export interface Post {
   frontmatter: Frontmatter;
   Content: () => unknown;
@@ -7,14 +17,26 @@ export interface Post {
 export type Posts = Post[];
 
 export async function getAllPosts(): Promise<Posts> {
-  const rawPosts = import.meta.glob('../posts/*.mdx', { eager: true });
+  const rawImports = import.meta.glob('../posts/*.mdx', { eager: true });
 
-  return Object.values(rawPosts).map((file: any) => {
-    // this will fail if frontmatter is missing
-    const frontmatter = FrontmatterSchema.parse(file.frontmatter);
-    return {
-      frontmatter,
-      Content: file.Content,
-    };
+  const unsortedPosts = Object.values(rawImports).map((file: unknown) => {
+    if (
+      isObject(file) &&
+      hasProp(file, 'frontmatter') &&
+      hasProp(file, 'Content')
+    ) {
+      // this will fail if frontmatter is missing
+      const frontmatter = FrontmatterSchema.parse(file.frontmatter);
+      return {
+        frontmatter,
+        Content: file.Content,
+      };
+    } else {
+      throw new Error('file missing required property');
+    }
   });
+
+  return unsortedPosts.sort(
+    (a, b) => b.frontmatter.date.getTime() - a.frontmatter.date.getTime()
+  );
 }
