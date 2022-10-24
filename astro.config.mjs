ae3092a75b8e7558b9359f136f0a84155ec1732b
jon kurinsky <jkurinsky@fastmail.com>
2022-10-23 21:32:43 -0400

    Revert "install image plugin"
    This reverts commit dd5dfe0b4e53dfe88af9aa916538d011543f4690.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
+
 import mdx from '@astrojs/mdx';
+
 import sitemap from '@astrojs/sitemap';
+
 import htmlMinifier from 'astro-html-minifier';
-import image from '@astrojs/image';
 
 export default defineConfig({
   site: 'https://krnsk0.dev',
-  integrations: [mdx(), sitemap(), htmlMinifier(), image()],
+  integrations: [mdx(), sitemap(), htmlMinifier()],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
