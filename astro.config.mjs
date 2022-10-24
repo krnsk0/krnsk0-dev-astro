dd5dfe0b4e53dfe88af9aa916538d011543f4690
jon kurinsky <jkurinsky@fastmail.com>
2022-10-23 21:28:11 -0400

    install image plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
-
 import htmlMinifier from 'astro-html-minifier';
+import image from '@astrojs/image';
 
 export default defineConfig({
   site: 'https://krnsk0.dev',
-  integrations: [mdx(), sitemap(), htmlMinifier()],
+  integrations: [mdx(), sitemap(), htmlMinifier(), image()],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
