d91ffa1aa51584890872e2cb6f152bc4f197c693
jon kurinsky <jkurinsky@fastmail.com>
2022-10-31 19:06:08 -0400

    integrate logrocket

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import htmlMinifier from 'astro-html-minifier';
-import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://krnsk0.dev',
-  integrations: [
-    mdx(),
-    sitemap(),
-    htmlMinifier(),
-    partytown({
-      forward: ['dataLayer.push'],
-    }),
-  ],
+  integrations: [mdx(), sitemap(), htmlMinifier()],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
