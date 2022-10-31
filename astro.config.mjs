4769bfa4ec469383d33099461c491d4f9bdd10b0
jon kurinsky <jkurinsky@fastmail.com>
2022-10-31 18:44:24 -0400

    set up ga4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
-
 import htmlMinifier from 'astro-html-minifier';
+import partytown from '@astrojs/partytown';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://krnsk0.dev',
-  integrations: [mdx(), sitemap(), htmlMinifier()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    htmlMinifier(),
+    partytown({
+      forward: ['dataLayer.push'],
+    }),
+  ],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
